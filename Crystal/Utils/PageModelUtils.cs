6eab4825e3656673a9f106d6ee2dda2c9a7722d0
potykion <potykion@gmail.com>
2018-05-06 19:51:00 +0300

    Some todos, untested link builders

--- Crystal/Utils/PageModelUtils.cs
+++ Crystal/Utils/PageModelUtils.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿using System;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal.Networking;
 
 namespace Crystal.Utils
 {
@@ -21,6 +23,41 @@ namespace Crystal.Utils
                 default:
                     return 1;
             }
+        }
+
+        static string BuildSubstanceLink(string language, string systemUrl, string method = "Index")
+        {
+            if (string.IsNullOrEmpty(systemUrl))
+            {
+                return $"/{language}/Substances/{method}";
+            }
+            else
+            {
+                if (method != "Edit" && method != "Create" && method != "Delete")
+                {
+                    method = "Details";
+                }
+
+                return $"/{language}/Substances/{method}/{systemUrl}";
+            }
+        }
+
+        // todo pass to href
+        static string BuildPropertyLink(string language, string systemUrl, string propertyUrl, string method = "Index")
+        {
+            if (method != "Edit" && method != "Create" && method != "Delete")
+            {
+                return $"/{language}/Substances/{propertyUrl}/{systemUrl}";
+            }
+
+            return $"/{language}/Substances/{propertyUrl}/{method}/{systemUrl}";
+        }
+
+        // todo language toggle link as ChangeLanguageUrl call
+        static string ChangeLanguageUrl(string url, string language)
+        {
+            var urlWithoutLanguage = url.Substring(url.IndexOf("/", 1, StringComparison.Ordinal));
+            return $"/{language}{urlWithoutLanguage}";
         }
     }
 }
