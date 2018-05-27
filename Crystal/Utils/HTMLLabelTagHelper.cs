ad5779725c8a5fbc987ad0557c07fc634a76c9c4
potykion <potykion@gmail.com>
2018-05-27 15:33:43 +0300

    Html in resources and labels

---
+++ Crystal/Utils/HTMLLabelTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Crystal.Utils
+{
+    [HtmlTargetElement("label", Attributes = ForAttributeName)]
+    public class HtmlLabelTagHelper : LabelTagHelper
+    {
+        private const string ForAttributeName = "asp-for";
+
+        public HtmlLabelTagHelper(IHtmlGenerator generator) :
+            base(generator)
+        {
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+            
+            var initialContent = output.Content.GetContent();
+            var contentAsHtml = WebUtility.HtmlDecode(initialContent);
+            
+            output.Content.Clear();
+            output.Content.AppendHtml(contentAsHtml);
+        }
+    }
+}
